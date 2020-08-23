2603d775c397b26b4603d820f40d0989a6efa89d
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-08-23 12:57:24 -0600

    made use of match props from react router

--- src/Containers/movieDetails.js
+++ src/Containers/movieDetails.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import MoviesList from './moviesList';
 import '../App.css';
 import Loading from '../Components/loading';
 import MoviePoster from '../Components/movie-poster';
 import Iframe from '../Components/iframe';
 import Logout from './logout';
-const MovieDetails = () => {
+import axios from 'axios';
+import { TMDBAPI } from '../env-variables';
+import { loading, notLoading } from '../Store/Redux/loading-reducer';
+import { getMovieDetails } from '../Store/actions';
+const MovieDetails = ({ match }) => {
 
-  const state = useSelector(state => state)
-  if (state.loading) return <Loading />
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+  const imdbID = match.params.id;
+
+  useEffect(() => {
+    dispatch(loading());
+    axios.get(`http://api.themoviedb.org/3/movie/${imdbID}?api_key=${TMDBAPI}&append_to_response=videos
+    `)
+      .then(response => (
+        dispatch(getMovieDetails(response.data))
+      ))
+      .then(() => dispatch(notLoading()))
+      .catch(error => console.log(error))
+
+  }, [imdbID, dispatch]);
 
   const movie = state.movie;
   const movies = state.movies;
-  const poster = movies.find(m => m.imdbID === movie.imdb_id).Poster
+  const poster = movies.find(m => m.imdbID === imdbID).Poster;
+
+  if (state.loader) return <Loading />
+
   return (
     <div>
       <div>
@@ -30,7 +50,7 @@ const MovieDetails = () => {
       <div>Language: {movie.original_language}</div>
       <div>Status: {movie.status}</div>
       <div>
-        <Iframe imdbKey={movie.videos.results[0]['key']} />
+        <Iframe imdbKey={movie?.videos?.results[0]['key']} />
       </div>
       <div >
         <MoviesList className="similar-movies" />
